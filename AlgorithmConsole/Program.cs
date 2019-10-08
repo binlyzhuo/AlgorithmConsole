04dffeb9660e758d400ebc85b5541aa827336a4f
binlyzhuo <binlyzhuo@outlook.com>
2019-10-08 20:47:59 +0800

    添加项目文件。

---
+++ AlgorithmConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AlgorithmConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+
+        static int BinarySearch()
+        {
+            return -1;
+        }
+    }
+}
